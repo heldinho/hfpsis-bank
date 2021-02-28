7f70b362b4bda279be0fbd523464b5959e290c1f
heldinho <helderferreirapassos@gmail.com>
2021-02-28 15:15:21 -0300

    fix
    Signed-off-by: heldinho <helderferreirapassos@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
-  <div id="nav">
-    <router-link to="/login">Login</router-link> |
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <Navbar />
+  <div class="mt-5">
+    <router-view />
   </div>
-  <router-view />
 </template>
+
+<script>
+import Navbar from '@/components/Navbar'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style lang="scss">
 body {
@@ -22,18 +29,5 @@ body {
   color: #2c3e50;
   height: 100vh;
   border: 0px solid #000;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,89 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand" href="javascript:">HFPSIS BANK</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarsExample04"
+      aria-controls="navbarsExample04"
+      aria-expanded="true"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="navbar-collapse collapse show" id="navbarsExample04" style="">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="javascript:">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="javascript:">Disabled</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="javascript:"
+            id="dropdown04"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+            @click="showDropdown"
+            >Dropdown</a
+          >
+          <div
+            class="dropdown-menu show"
+            aria-labelledby="dropdown04"
+            v-if="dropdown"
+          >
+            <a class="dropdown-item" href="javascript:" @click="handleClose"
+              ><router-link class="links" to="/">Home</router-link></a
+            >
+            <a class="dropdown-item" href="javascript:" @click="handleClose"
+              ><router-link class="links" to="/login">Login</router-link></a
+            >
+            <a class="dropdown-item" href="javascript:" @click="handleClose"
+              ><router-link class="links" to="/about">About</router-link></a
+            >
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  data: () => ({
+    dropdown: false
+  }),
+  beforeCreate() {
+    document.title = '</ HFPSIS > - Home'
+  },
+  methods: {
+    showDropdown() {
+      this.dropdown = !this.dropdown
+    },
+    handleClose() {
+      this.dropdown = false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dropdown-menu {
+  margin-top: 7px;
+  border-radius: 0px 0px 4px 4px;
+  .links {
+
+  }
+}
+</style>
